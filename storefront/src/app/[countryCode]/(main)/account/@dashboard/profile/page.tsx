aa80d5f955af968698a0d1984c49e93d8af7c5bf
Andres Hernandez <ra.hernandezc@duocuc.cl>
2025-10-18 02:05:28 -0300

    change names medusa to lentes de sol

--- storefront/src/app/[countryCode]/(main)/account/@dashboard/profile/page.tsx
+++ storefront/src/app/[countryCode]/(main)/account/@dashboard/profile/page.tsx
@@ -11,8 +11,8 @@ import { listRegions } from "@lib/data/regions"
 import { getCustomer } from "@lib/data/customer"
 
 export const metadata: Metadata = {
-  title: "Profile",
-  description: "View and edit your Medusa Store profile.",
+  title: "Perfil",
+  description: "Revisa y edita tu perfil de LENTES DE SOL",
 }
 
 export default async function Profile() {
